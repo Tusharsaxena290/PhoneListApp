377fddaaae825243839a8078db5cd8bdf99ad06e
Tushar Saxena <tushar.saxena_iot18@gla.ac.in>
2020-02-22 12:09:14 +0530

    implements the methods of the adt

--- src/definition/ContactLinkedList.java
+++ src/definition/ContactLinkedList.java
@@ -1,4 +1,20 @@
 package definition;
 
-public class ContactLinkedList {
+import adt.javaAdt;
+
+public class ContactLinkedList<contact> implements javaAdt<contact> {
+    @Override
+    public boolean add(contact dataItem) {
+        return false;
+    }
+
+    @Override
+    public boolean remove() {
+        return false;
+    }
+
+    @Override
+    public void viewAllContacts() {
+
+    }
 }
