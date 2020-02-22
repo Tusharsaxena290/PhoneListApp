d6487e8fa4b20f8d17761a0c45bf619784acc0b7
Tushar Saxena <tushar.saxena_iot18@gla.ac.in>
2020-02-22 11:34:34 +0530

    create the constructor

--- src/definition/contact.java
+++ src/definition/contact.java
@@ -1,8 +1,18 @@
 package definition;
 
+import java.util.ArrayList;
+
 public class contact {
     String FirstNmae;
     String LastName;
     String Email;
+    ArrayList<String> Phonenumber;
 
+
+    public contact(String firstNmae, String lastName, String email, ArrayList<String> phonenumber) {
+        FirstNmae = firstNmae;
+        LastName = lastName;
+        Email = email;
+        Phonenumber = phonenumber;
+    }
 }
