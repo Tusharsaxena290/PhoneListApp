3a0568816c12596b92c1a1dd73607a047d1c3b00
Tushar Saxena <tushar.saxena_iot18@gla.ac.in>
2020-02-22 11:36:31 +0530

    create getters and setters

--- src/definition/contact.java
+++ src/definition/contact.java
@@ -3,16 +3,49 @@ package definition;
 import java.util.ArrayList;
 
 public class contact {
-    String FirstNmae;
+    String FirstName;
     String LastName;
     String Email;
     ArrayList<String> Phonenumber;
 
 
-    public contact(String firstNmae, String lastName, String email, ArrayList<String> phonenumber) {
-        FirstNmae = firstNmae;
+    public contact(String firstName, String lastName, String email, ArrayList<String> phonenumber) {
+        FirstName = firstName;
         LastName = lastName;
         Email = email;
         Phonenumber = phonenumber;
     }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public ArrayList<String> getPhonenumber() {
+        return Phonenumber;
+    }
+
+    public void setPhonenumber(ArrayList<String> phonenumber) {
+        Phonenumber = phonenumber;
+    }
+
 }
