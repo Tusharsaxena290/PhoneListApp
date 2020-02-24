35ab66382c80e960b618f1611e9b7cd0b3128488
Tushar Saxena <tushar.saxena_iot18@gla.ac.in>
2020-02-24 18:06:10 +0530

    taking input from the user

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -4,9 +4,11 @@ import definition.ContactLinkedList;
 import definition.contact;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
 
         String firstName;
@@ -17,7 +19,30 @@ public class Main {
         ContactLinkedList<contact> contactList = new ContactLinkedList<>();
         ArrayList<String> fullName = new ArrayList<>();
         while (true) {
-            System.out.println("Welcome to Tushar's Contact List Application\n");
+            System.out.println("Welcome to Tushar's Contact List Application\n" +
+                    "Press 1 to add the new contact\n" +
+                    "Press 2 to view the contacts\n" +
+                    "Press 3 to search a contact" +
+                    "Press 4 to delete a contact" +
+                    "Press 5 to exit the program");
+            select = sc.nextInt();
+            if (select == 1 || select == 2 || select == 3 || select == 4) {
+                switch (select) {
+                    case 1:
+                        System.out.println("You have chosen to add a new contact:");
+                        sc.nextLine();
+                        System.out.println("enter the name:");
+                        sc.nextLine();
+                        System.out.println("First Name:");
+                        firstName = sc.nextLine();
+                        System.out.println("Last Name:");
+                        lastName = sc.nextLine();
+                        fullName.add(firstName + lastName);
+
+
+                }
+            }
+
         }
     }
 }
