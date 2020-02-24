fd81bf9a634c8c471bd89de167edcc41bb47d6f1
Tushar Saxena <tushar.saxena_iot18@gla.ac.in>
2020-02-24 17:53:35 +0530

    creating fields

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,4 +1,20 @@
 package execution;
 
+import definition.ContactLinkedList;
+import definition.contact;
+
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+        String firstName;
+        String lastName;
+        String email;
+        char choose;
+        int select;
+        ContactLinkedList<contact> contactList = new ContactLinkedList<>();
+        ArrayList<String> fullName = new ArrayList<>();
+    }
 }
